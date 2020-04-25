23fadde51dd1f3b07a589c8eaf0f6de1d66bbab1
fumio68 <rgkk.5123@gmail.com>
2020-04-25 13:07:11 +0900

    thisとbind

---
+++ 03_JavaScript/120_thisの使い方/main.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function(){
+  const btn = document.querySelector('#btn');
+  const ta = new TextAnimation('.animate-title');
+  ta.animate();
+  btn.addEventListener('click', ta.animate.bind(ta));
+});
+
+class TextAnimation {
+  constructor(el){
+    this.el = document.querySelector(el);
+    this.chars = this.el.innerHTML.trim().split("");
+    this.el.innerHTML = this._splitText();
+  }
+  _splitText(){
+    return this.chars.reduce((acc, curr) => {
+      curr = curr.replace(' ', '&nbsp;');
+      return `${acc}<span class="char">${curr}</span>`;
+    }, "");
+  }
+  animate(){
+    this.el.classList.toggle('inview');
+  }
+}
